787e3e95f9b1c6093f68c68e61433ea2f5b0912e
Tsien Jin <tsien.jin@gmail.com>
2024-01-14 17:44:44 +0800

    Main GUI and logic running

--- application/app.py
+++ application/app.py
@@ -5,26 +5,39 @@ from dotenv import load_dotenv
 
 from application.database.db_driver import DbDriver
 from application.database.image_driver import ImageDriver
+from application.database.source_driver import SourceDriver
 from application.objects.image import Image
 from application.scan.probe import Probe, ProbeArgs
 from application.util.image_dumper import ImageDumper
-
-
+from application.gui.controller import GuiController
+from application.util.logger import Logger
 
 load_dotenv(".env")
 
 
 def main():
 
+    # imageDriver = ImageDriver()
+    #
+    # probeArgs = ProbeArgs(
+    #     path=os.environ.get("IMG_PARENT_FOLDER"),
+    #     regex_filter=re.compile(r'.*.(jpg|jpeg|png|cr2)', re.IGNORECASE)
+    # )
+    # probe = Probe(probeArgs)
+    # image_tree = probe.get_all_dir()
+    #
+    # ImageDumper.dump_from_scantree(image_tree, imageDriver)
+    #
+    # print(image_tree)
+
+    sourceDriver = SourceDriver()
     imageDriver = ImageDriver()
 
-    probeArgs = ProbeArgs(
-        path=os.environ.get("IMG_PARENT_FOLDER"),
-        regex_filter=re.compile(r'.*.(jpg|jpeg|png|cr2)', re.IGNORECASE)
-    )
-    probe = Probe(probeArgs)
-    image_tree = probe.get_all_dir()
-
-    ImageDumper.dump_from_scantree(image_tree, imageDriver)
+    logger = Logger()
+    logger.emit("Starting application", fallback_source="APP.PY")
+    gui = GuiController()
+    logger.emit("Quiting application", fallback_source="APP.PY")
 
 
+if __name__ == "__main__":
+    main()
