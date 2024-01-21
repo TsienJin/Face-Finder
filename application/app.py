b3d1331af91a3c0aa5e00ef4bce78769f74d7d79
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-01-21 21:39:51 +0800

    Debugging changes

--- application/app.py
+++ application/app.py
@@ -1,12 +1,15 @@
 import os
 import re
 
+import numpy as np
 from dotenv import load_dotenv
 
+from application.business_logic.biz_scan_for_faces import BizScanForFaces
 from application.database.db_driver import DbDriver
 from application.database.image_driver import ImageDriver
 from application.database.source_driver import SourceDriver
-from application.objects.image import Image
+from application.models.faceInImage import FaceInImage
+from application.models.image import Image
 from application.scan.probe import Probe, ProbeArgs
 from application.util.image_dumper import ImageDumper
 from application.gui.controller import GuiController
@@ -16,15 +19,21 @@ load_dotenv(".env")
 
 
 def main():
+    # logger = LogEmitter("APP.PY")
+    # logger.emit("Starting application")
+    # gui = GuiController()
+    # logger.emit("Quiting application")
 
     # Initialise tables
-    sourceDriver = SourceDriver()
+    # sourceDriver = SourceDriver()
     imageDriver = ImageDriver()
+    # print(imageDriver.get_all_images())
+    bizScanFace = BizScanForFaces()
+    bizScanFace.scan_indexed_files()
+    #
 
-    logger = LogEmitter("APP.PY")
-    logger.emit("Starting application")
-    gui = GuiController()
-    logger.emit("Quiting application")
+
+    # fim = FaceInImage(encoding=np.array([1,2,3,]))
 
 
 if __name__ == "__main__":
