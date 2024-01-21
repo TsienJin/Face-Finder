1b2aa7e95918815f7af38d89112d6dc0a2722e28
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-01-21 21:40:22 +0800

    Added "Array" adapter for SQLite3

--- application/database/db_driver.py
+++ application/database/db_driver.py
@@ -1,5 +1,7 @@
 import os
 import sqlite3
+import numpy as np
+import io
 
 
 class DbDriver:
@@ -9,8 +11,27 @@ class DbDriver:
     def __init__(self):
         self.name="Driver"
 
+        """
+        https://stackoverflow.com/questions/18621513/python-insert-numpy-array-into-sqlite3-database
+        """
+        sqlite3.register_adapter(np.ndarray, DbDriver.__np_arr_to_text)
+        sqlite3.register_converter("array", DbDriver.__text_to_np_arr)
+
     def __init_table(self):
         raise NotImplementedError
+
+    @staticmethod
+    def __np_arr_to_text(arr):
+        out = io.BytesIO()
+        np.save(out, arr)
+        out.seek(0)
+        return sqlite3.Binary(out.read())
+
+    @staticmethod
+    def __text_to_np_arr(text):
+        out = io.BytesIO(text)
+        out.seek(0)
+        return np.load(out)
 
     def connect(self):
         self.connection = sqlite3.connect(os.environ.get("DB_PATH"))
